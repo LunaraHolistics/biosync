c21882dcd9818db2a6f084adcf270ed32edb9605
LunaraHolistics <celsot.holistics@gmail.com>
2026-04-03 18:24:38 -0300

    atualização dashboard

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,6 +1,6 @@
 import jsPDF from "jspdf";
 import { useEffect, useState } from "react";
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from "../lib/supabaseclient";
 
 type Exame = {
     id: string;
